05e4949fd4be622aed0613a7f7e025600a7ba6fa
Gttolla <lucas12chves@gmail.com>
2025-10-28 13:58:45 -0300

    :sparkles: feat: scores DELETE dynamic route added

---
+++ app/api/scores/[id]/route.ts
@@ -0,0 +1,30 @@
+import { NextResponse } from "next/server";
+import { db } from "@/app/lib/firebase";
+import { doc, deleteDoc, getDoc } from "firebase/firestore";
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    const { id } = await params;
+
+    if (!id) {
+      return NextResponse.json({ message: "Missing required params" }, { status: 400 });
+    }
+
+    const scoreRef = doc(db, "scores", id);
+    const scoreSnapshot = await getDoc(scoreRef);
+
+    if (!scoreSnapshot.exists()) {
+      return NextResponse.json({ message: "Score not found" }, { status: 404 });
+    }
+
+    await deleteDoc(scoreRef);
+
+    return NextResponse.json({ message: "Score deleted successfully"});
+  } catch (error) {
+    console.error("Error deleting score:", error);
+    return NextResponse.json({ error: "Error deleting score" }, { status: 500 });
+  }
+}
